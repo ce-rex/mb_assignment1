fdff304795b153e2e45408c892a4ede97ab412e2
Tanja Eichner <tanjaeichner@MacBook-Pro-de-Tanja.local>
2020-05-22 12:56:11 +0200

    Unterraum-Projektion Rekonstruktionsfehler hinzugefügt, Plots angepasst und Untersuchungen für Nebenvektor hinzugefügt.

--- RUN.m
+++ RUN.m
@@ -48,6 +48,12 @@ D = data.data3;
 %PCA
 [eigval, eigvec] = pca(D);
 
+figure(1)
+scatter(D(1,:), D(2,:))
+title('Original data3 in 2D space');
+
+%%% Main vector
+
 %Projection
 data_projected = projection(eigvec(:,1), D);
     
@@ -55,20 +61,37 @@ figure(2)
 scatter(data_projected(1,:), zeros(1, 50))
 title('Projection of data3 to 1D space (main vector)');
 
-figure(3)
-scatter(D(1,:), D(2,:))
-title('Original data3 in 2D space');
-
 %Reconstruction
 mean_orig = mean(D, 2);
 data_reconstructed = reconstruction(eigvec(:,1), data_projected, mean_orig); 
 
-%figure(4)
-%scatter(data_reconstructed(1,:), data_reconstructed(2,:))
-%title('Reconstruction data3 (main vector)');
+figure(3)
+scatter(data_reconstructed(1,:), data_reconstructed(2,:))
+title('Reconstruction data3 (main vector)');
 
 plot2DPCA(D', mean_orig, data_reconstructed', eigvec, eigval, 1, 1);
 
+reconstruction_error = immse(D, data_reconstructed)
+
+
+%%% Side vector
+data_projected_side = projection(eigvec(:,2), D);
+    
+figure(5)
+scatter(data_projected_side(1,:), zeros(1, 50))
+title('Projection of data3 to 1D space (side vector)');
+
+%Reconstruction
+mean_orig_side = mean(D, 2);
+data_reconstructed_side = reconstruction(eigvec(:,2), data_projected_side, mean_orig_side); 
+
+figure(6)
+scatter(data_reconstructed(1,:), data_reconstructed(2,:))
+title('Reconstruction data3 (side vector)');
+
+plot2DPCA(D', mean_orig_side, data_reconstructed_side', eigvec, eigval, 1, 1);
+
+reconstruction_error_side = immse(D, data_reconstructed_side)
 
 
 %% Untersuchungen in 3D
