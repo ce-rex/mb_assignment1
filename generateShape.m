fec0f51523cea9dfe6cfbb51245a5467f0ac003a
Tanja Eichner <tanjaeichner@MacBook-Pro-de-Tanja.local>
2020-05-22 16:43:23 +0200

    GenerateShape Funktion hinzugefügt

---
+++ generateShape.m
@@ -0,0 +1,8 @@
+function [shapes] = generateShape(b, eigenvectors)
+%GENERATESHAPE takes a parameter vector b as input and computes new shapes, where the length of b indicates the number of Eigenvectors to be considered for shape generation
+%   b = parameter vector
+%   eigenvectors = eigenvectors to be used for shape generation
+
+shapes = b;
+end
+
