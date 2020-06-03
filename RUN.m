ec1b7be24f83201ffb5133f44cab51475b9b564c
Marian Stoschitzky <marian.sto24@gmail.com>
2020-06-03 17:35:23 +0200

    Add multi-param shape gen in script.

--- RUN.m
+++ RUN.m
@@ -9,7 +9,37 @@ addpath(providedFunctions_path);
 
 %% shape model
 
-% code
+data = load(data_path + "/shapes.mat");
+shapes = data.aligned;
+
+% Flatten shapes so that ourPca can process them
+shapes_flattened = reshape(shapes, 256, 14);
+
+% Perform pca on the flattened shapes
+[eigval, eigvec] = ourPca(shapes_flattened);
+eigval_sqrts = sqrt(eigval);
+
+% try different parameters to use with generate_shape
+b_coefficients = [
+ 0 1 2 -1 -2;
+ 2 -1 0 3 0;
+ -1 2 2 -2 -2;
+];
+r = [0, 45, 90];
+s = [1, 0.5, 3];
+x = [0 50 10];
+y = [0 50 -50];
+
+figure()
+title("Shapes generated based on different parameter settings");
+for i = [1, 2, 3]
+    
+    generated_shape = generateShape(eigval_sqrts(1:5) .* b_coefficients(i, :)', r(i), s(i), x(i), y(i));
+
+    % Plot generated shape
+    subplot(1, 3, i);
+    scatter(generated_shape(:, 1), generated_shape(:, 2));
+end
 
 %% feature extraction
 
