f043407ccd52a9c02f043f196a4ee653bd64a1a3
Marian Stoschitzky <marian.sto24@gmail.com>
2020-06-03 16:21:52 +0200

    Add new RUN.m for assignment 2.

---
+++ RUN.m
@@ -0,0 +1,12 @@
+close all;
+clc;
+
+% Defines and adds data path
+data_path = "beispiel_pca_material";
+addpath(data_path);
+providedFunctions_path = "providedFunctions";
+addpath(providedFunctions_path);
+
+%% generateShape
+
+% hello
