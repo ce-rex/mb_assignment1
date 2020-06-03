4ec6681a8fb064a41b7c8346509bd3016c0bcab5
Marian Stoschitzky <marian.sto24@gmail.com>
2020-06-03 16:13:40 +0200

    Add r,s,x,y to generate_shape

--- generateShape.m
+++ generateShape.m
@@ -1,6 +1,10 @@
-function [generated_shape] = generateShape(b)
+function [transformed_shape] = generateShape(b, r, s, x, y)
 %GENERATESHAPE takes a parameter vector b as input and computes new shapes, where the length of b indicates the number of Eigenvectors to be considered for shape generation
 %   b = parameter vector
+%   r = rotation angle in degrees
+%   s = scaling factor
+%   x = translation distance on x-axis
+%   y = translation distance on y-axis
 
 % load data
 addpath('beispiel_pca_material');
@@ -24,5 +28,15 @@ generated_shape = reconstruction(eigvec(:,1:n_parameters), b, mean_coordinate);
 
 % reshape into 2-dimensional array
 generated_shape = reshape(generated_shape, 128, 2);
+
+% create scaling matrix
+scaleMat = [s 0; 0 s];
+% create rotation matrix
+rotMat = [ cosd(r) -sind(r); sind(r) cosd(r)];
+% create translation matrix
+transMat = [x; y];
+
+% apply matrix transformations on generated_shape
+transformed_shape = (rotMat * scaleMat * generated_shape' + transMat)';
 end
 
