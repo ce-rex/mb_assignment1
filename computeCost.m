ab82b27098944630fac7fb001dd5958dc1a8a68c
Tanja Eichner <tanjaeichner@MacBook-Pro-de-Tanja.local>
2020-06-08 16:27:40 +0200

    Kostenfunktion angepasst

--- computeCost.m
+++ computeCost.m
@@ -1,22 +1,31 @@
-function totalcost = computeCost( probability_map, p, eigvec, mean_shape )
-% generated_shape = [shapesmean,shapeseVal,shapeseVec] .. Array der
-%PCA der Trainingsbilder (1-30)
-%p=[r,s,tx,ty].. Parametervektor(r=rotation, s=skalierung, tx=translation
-%in x, ty=translation in y)
-%contscore.. Wahrscheinlichkeit das ein Pixel im Hintergrund liegt.
+function total_costs = computeCost(probability_map, p, eigen_vectors, mean_shape)
+% COMPUTECOST Returns a scalar value representing the costs which describe 
+% how well the (from p) generated shapes fit the classification result.
+%   probability_map: classification result
+%   p: parameter vector, p = (scaling, rotation, x?translation,
+%   y?translation, b)
+%   eigen_vectors: Eigen vectors to limit the number of modes
+%   total_costs: returned costs as a sum of the costs of the single points
 
-b = ones(sum((eigvec(:,2)/sum(eigvec(:,2)))>0.001),1); %nur jene Modes verwenden die mindest 0.1% der Gesamtvarianz beitragen.
-generated_shape = generateShape(b, eigvec(:,3:end), eigvec(:,1)', p(1), p(2), p(3), p(4));
+b = p(5:length(p));
+generated_shape = generateShape(b, eigen_vectors, mean_shape, p(1), p(2), p(3), p(4));
 
-totalcost=0;
+total_costs=0;
 for i=1:size(generated_shape,2) %Schleifenberechnung ueber alle Punkte
-    [c1,c2,contprob]=improfile(probability_map,generated_shape(1,i),generated_shape(2,i)); %contprob.. Wahrscheinlichkeit, dass der Punkt im Hintergrund liegt( also im Intervall [0,1])
-    if isnan(contprob)
-        cost=100000; %wenn contprob=NaN, dann werden enorm hohe Kosten aufgeschlagen (100000), denn dann liegt der Punkt nicht mal auf dem Klassifikatorergebnis/Pixelbild
-    else
-        cost=contprob*1000; %den Wahrscheinlichkeiten Kosten zuordnen, desto hoeher die Wahrscheinlichkeit, dass der Punkt im Hintergrund liegt, desto hoehere Kosten aufschlagen.
+    [c1,c2,probability] = improfile(probability_map,generated_shape(1,i),generated_shape(2,i)); %contprob.. Wahrscheinlichkeit, dass der Punkt im Hintergrund liegt( also im Intervall [0,1])
+    % If contprob is not a number, the shape is bigger than the
+    % probability map. In this case, contprob is set to 1 since the
+    % probability that this point is part of the background is 100%.
+    % Idea for better approach: distance measure (denn wenn der Punkt auch
+    % nur 1 Pixel daneben ist, ist die Wahrscheinlichkeit der
+    % Übereinstimmung gleich 0)
+    if isnan(probability)
+        probability = 1; 
+    % The higher the probability that the point is part of the background,
+    % the higher the costs (*1000 for easier numbers)
+    cost = probability * 1000; 
     end
-    totalcost=totalcost+cost; %Die Kosten des einzelnen Punktes den Gesamtkosten aufschlagen
+    total_costs = total_costs + cost; 
 end
 
 end
