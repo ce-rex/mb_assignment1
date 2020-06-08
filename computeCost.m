651812a7637cfe9e59ebcc5481aa9fd986cd0a9c
Tanja Eichner <tanjaeichner@MacBook-Pro-de-Tanja.local>
2020-06-08 16:28:58 +0200

    Alte Kommentare entfernt

--- computeCost.m
+++ computeCost.m
@@ -11,8 +11,8 @@ b = p(5:length(p));
 generated_shape = generateShape(b, eigen_vectors, mean_shape, p(1), p(2), p(3), p(4));
 
 total_costs=0;
-for i=1:size(generated_shape,2) %Schleifenberechnung ueber alle Punkte
-    [c1,c2,probability] = improfile(probability_map,generated_shape(1,i),generated_shape(2,i)); %contprob.. Wahrscheinlichkeit, dass der Punkt im Hintergrund liegt( also im Intervall [0,1])
+for i=1:size(generated_shape,2) 
+    [c1,c2,probability] = improfile(probability_map,generated_shape(1,i),generated_shape(2,i)); 
     % If contprob is not a number, the shape is bigger than the
     % probability map. In this case, contprob is set to 1 since the
     % probability that this point is part of the background is 100%.
